e5b87a38a0b300e115453d04e291da36c2aaa56f
Janith Sandamal <78975250+Janith-Sandamal@users.noreply.github.com>
2023-02-28 12:01:46 +0530

    implement stack push method

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,26 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("Hello world!");
+        class stack
+        {
+            private double []stkArray;
+            private int maxSize;
+            private int top;
+
+            public stack(int S)
+            {
+                maxSize = S;
+                stkArray = new double[maxSize];
+                top = -1;
+            }
+
+            public void push(double j)
+            {
+                if(top == maxSize-1)
+                    System.out.println("Stack is full");
+                else
+                    stkArray[++top]=j;
+            }
+        }
     }
 }
